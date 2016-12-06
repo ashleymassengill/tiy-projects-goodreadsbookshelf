fdab12acabfcb7d1940c97d65c32dac62add3cb3
Ashley Massengill <massengillashley@icloud.com>
2016-12-06 14:38:34 -0500

    js update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,43 @@
 $(document).ready(function(event){
   $(".fa-bars").click(function(){
     $(".mainnav li").slideToggle("slow");
+    console.log ("nav toggle working");
   });
 });
+
+$(document).ready(function(event){
+  $(window).resize(function(){
+    if ($(window).width() <= 767){
+      $(".book").click(function(){
+        $(".hover").slideToggle("slow");
+        console.log (".hover toggle working");
+      });
+    };
+  });
+});
+
+// $(document).ready(function(event){
+//   $(window).resize(function(){
+//     if ($(window).width(>=768) && $(window).width(<=1024)){
+//       $(".book img").click(function(){
+//         $(".hover").slideToggle("fast");
+//       });
+//     };
+//   });
+// });
+
+//
+// $(document).ready(function() {
+//     // run test on initial page load
+//     checkSize();
+//
+//     // run test on resize of the window
+//     $(window).resize(checkSize);
+// });
+//
+// //Function to the css rule
+// function checkSize(){
+//     if ($(".sampleClass").css("float") == "none" ){
+//         // your code here
+//     }
+// }
